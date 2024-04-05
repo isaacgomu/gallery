0f05d006b8c616bda19f5091670236422fd202ea
isaacgomu <IsaacDalglish03@gmail.com>
2024-04-05 19:13:38 +0100

    i have a slight idea of what im doing

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const mainImage = document.getElementById("mainimage");
+const mainImage = document.getElementById("mainImage");
 const thumbnails = document.getElementById("thumbnails");
-
+const fwrd = document.getElementById("fwrd");
 const gallery = [
   {
-    url: "./assets/chillblowfish.jpg",
+    url: "./assets/chill blowfish.jpg",
     alt: "A pufferfish, very puffed up, held in the hand of someone.",
   },
   {
-    url: "./assets/chillblowfish2.jpg",
+    url: "./assets/chill blowfish2.jpg",
     alt: "A pufferfish, deflated, held in the hand of someone.",
+  },
+  {
+    url: "./assets/chill blowfish3.jpg",
+    alt: "A puffed up pufferfish with its face towards the camera, giving a surprised expression.",
+  },
+  {
+    url: "./assets/chill blowfish4.jpg",
+    alt: "A spiky puffed up pufferfish swimming in the ocean.",
   },
 ];
 
@@ -32,7 +40,37 @@ function createMainImage(image) {
   mainImageElement.alt = image.alt;
 
   mainImage.appendChild(mainImageElement);
+
+  if (gallery.indexOf(image) == gallery.length - 1) {
+    fwrd.disabled = true;
+  } else {
+    fwrd.disabled = false;
+  }
+
+  if (gallery.indexOf(image) === 0) {
+    bwrd.disabled = true;
+  } else {
+    bwrd.disabled = false;
+  }
 }
+
+let indexNumber = 0;
+
+function imagebutton(index) {
+  indexNumber += index;
+
+  if (indexNumber >= gallery.length) indexNumber = 0;
+  if (indexNumber < 0) indexNumber = gallery.length - 1;
+  console.log(indexNumber);
+  createMainImage(gallery[indexNumber]);
+}
+fwrd.addEventListener("click", function () {
+  imagebutton(1);
+});
+
+bwrd.addEventListener("click", function () {
+  imagebutton(-1);
+});
 
 createThumbnails();
 createMainImage(gallery[0]);
